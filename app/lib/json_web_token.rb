ac4405207791d19a0f6b5a56c1ad4a122476666c
Joe Palmieri <jpalmieri@lumoslabs.com>
2018-04-16 13:58:45 -0700

    add authenticate user

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,6 +1,6 @@
 class JsonWebToken
   # secret to encode and decode token
-  HMAC_SECRET = "1234"
+  HMAC_SECRET = Rails.application.credentials.secret_key_base
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
