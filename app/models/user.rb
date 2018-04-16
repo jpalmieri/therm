add4277011a0e2141daa1bfa25740a2860ed0dfd
Joe Palmieri <jpalmieri@lumoslabs.com>
2018-04-16 16:46:33 -0700

    remove 'Password digest can't be blank' validation message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password # encrypt password
   has_many :projects, dependent: :destroy
   validates_presence_of :name, :email, :password_digest
-  has_secure_password # encrypt password
 end
